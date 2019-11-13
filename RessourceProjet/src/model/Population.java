2fa0d2349b1fecd69d461f736cb5a08f0cbacd6d
Pierre <pi.billaud@gmail.com>
2019-11-13 15:51:41 +0100

    class Population

--- RessourceProjet/src/model/Population.java
+++ RessourceProjet/src/model/Population.java
@@ -7,15 +7,24 @@ public class Population {
 
     ArrayList<Circuit> list;
 
-    public Population() {
+    public Population(GestionAllCircuit gestionnaireCircuit, int populationSize, boolean init) {
+
         this.list = new ArrayList<>();
+
+        if(init) {
+            for (int i = 0; i< populationSize; i++) {
+                Circuit newCircuit =new Circuit();
+            }
+        }
+
     }
 
     public void addCircuit(Point[] points){
         Circuit circuit = new Circuit();
-        circuit.addListPoints(points);
+        circuit.GestionAllCircuit.addListPoints(points);
         list.add(circuit);
     }
+
     public void addListCircuits(Circuit[] circuits){
         list.addAll(Arrays.asList(circuits));
     }
@@ -23,7 +32,32 @@ public class Population {
     public ArrayList<Circuit> getList() {
         return list;
     }
+
     public void setList(ArrayList<Circuit> list) {
         this.list = list;
+    }
+
+    public Circuit getCircuit(int index) {
+        return list.get(index);
+    }
+
+    public void saveCircuit(int index, Circuit circuit) {
+        list.add(index, circuit);
+    }
+
+    public Circuit getFittest() {
+        Circuit fittest = list.get(0);
+
+        for (int i = 0; i < populationSize(); i++) {
+            if(fittest.getFitness() <= getCircuit(i).getFitness()) {
+                fittest = getCircuit(i);
+            }
+        }
+
+        return fittest;
+    }
+
+    public int populationSize() {
+        return list.size();
     }
 }
