c49aa239667ef0ab6e4fb1ee895b323e8eb129b5
VictorCouturieux <victor.couturieux@sas-impact.fr>
2019-11-13 14:37:40 +0100

    point circuit population

--- RessourceProjet/src/model/Circuit.java
+++ RessourceProjet/src/model/Circuit.java
@@ -1,4 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Circuit {
+
+    ArrayList<Point> list;
+
+    public Circuit() {
+        list = new ArrayList<>();
+    }
+
+    public void addPoint(int x, int y){
+        list.add(new Point(x, y));
+    }
+    public void addListPoints(Point[] points){
+        list.addAll(Arrays.asList(points));
+    }
+
+    public ArrayList<Point> getList() {
+        return list;
+    }
+    public void setList(ArrayList<Point> list) {
+        this.list = list;
+    }
 }
--- RessourceProjet/src/model/Population.java
+++ RessourceProjet/src/model/Population.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Population {
+
+    ArrayList<Circuit> list;
+
+    public Population() {
+        this.list = new ArrayList<>();
+    }
+
+    public void addCircuit(Point[] points){
+        Circuit circuit = new Circuit();
+        circuit.addListPoints(points);
+        list.add(circuit);
+    }
+    public void addListCircuits(Circuit[] circuits){
+        list.addAll(Arrays.asList(circuits));
+    }
+
+    public ArrayList<Circuit> getList() {
+        return list;
+    }
+    public void setList(ArrayList<Circuit> list) {
+        this.list = list;
+    }
 }
